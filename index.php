95143ad5500aeaf527b830ea1ef31366f78d24ff
rocketpastsix <mjftrask@gmail.com>
2014-12-27 11:29:45 -0500

    added Bootstrap, Roboto Font, Styles.css

--- index.php
+++ index.php
@@ -11,6 +11,9 @@
 
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,100' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/styles.css" type="text/css" rel="stylesheet">
         <script src="js/vendor/modernizr-2.8.0.min.js"></script>
     </head>
     <body>
@@ -19,7 +22,7 @@
         <![endif]-->
 
         <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <h3>Hello world! This is HTML5 Boilerplate.</h3>
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
