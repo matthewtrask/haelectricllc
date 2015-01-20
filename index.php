35d338c2064f15ea1d0cb86ce5ab7386457a345e
rocketpastsix <mjftrask@gmail.com>
2015-01-19 21:55:41 -0500

    updated html, started build

--- index.php
+++ index.php
@@ -1,26 +1,62 @@
+<?php $siteTitle = "HA Electric LLC"; 
+$pageTitle = "Commerical &amp; Residential Electric Work"; ?>
+
 <!doctype html>
 <html class="no-js" lang="">
     <head>
         <meta charset="utf-8">
+        <meta author="Matt Trask">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>HA Electric LLC | Commerical &amp; Residential Electric Work</title>
+        <title><?php echo $siteTitle; ?> | <?php echo $pageTitle ?></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
 
         <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
-
+        
+        <!-- Stylesheets -->
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
         <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,100' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="css/styles.css" type="text/css" rel="stylesheet">
+        
+        <!-- Scripts -->
         <script src="js/vendor/modernizr-2.8.0.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 8]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
+        <!-- Nav bar Start -->
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display"-->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navmenu"><!--Targets the id on the menu -->
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="#" class="navbar-brand"><?php echo $siteTitle; ?></a>
+                </div>
+
+                <div class="collapse navbar-collapse navbar-right" id="navmenu">
+                    <ul class="collapse navbar-collapse">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">services</a></li>
+                        <li><a href="#">Testimonials</a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            
+        </nav>
 
         <!-- Add your site or application content here -->
         
