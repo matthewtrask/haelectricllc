724d2b386d06bd8ae38bf14fdff70f37070bfeb9
rocketpastsix <mjftrask@gmail.com>
2015-01-31 11:08:22 -0500

    moved head into include folder

--- public/includes/head.php
+++ public/includes/head.php
@@ -1,1 +1,25 @@
-<?php
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta author="Matt Trask">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title><?php echo $siteTitle; ?> | <?php echo $pageTitle; ?></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
+
+        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+        
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,100' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/styles.css" type="text/css" rel="stylesheet">
+        
+        <!-- Scripts -->
+        <script src="js/vendor/modernizr-2.8.0.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    </head>
--- public/index.php
+++ public/index.php
@@ -1,31 +1,7 @@
 <?php $siteTitle = "HA Electric LLC"; 
 $pageTitle = "Commerical &amp; Residential Electric Work"; ?>
 
-<!doctype html>
-<html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta author="Matt Trask">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title><?php echo $siteTitle; ?> | <?php echo $pageTitle; ?></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
-
-        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
-        
-        <!-- Stylesheets -->
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-        <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,100' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="css/styles.css" type="text/css" rel="stylesheet">
-        
-        <!-- Scripts -->
-        <script src="js/vendor/modernizr-2.8.0.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-    </head>
+<?php include('includes/head.php'); ?>
     <body>
         <!--[if lt IE 8]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
