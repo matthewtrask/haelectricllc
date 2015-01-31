d92dc0b176cffc473d1ca867aebe7f391ed94944
rocketpastsix <mjftrask@gmail.com>
2015-01-31 11:14:52 -0500

    moved nav to nav include file.

--- public/index.php
+++ public/index.php
@@ -7,32 +7,7 @@ $pageTitle = "Commerical &amp; Residential Electric Work"; ?>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         <!-- Nav bar Start -->
-        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-            <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display"-->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navmenu"><!--Targets the id on the menu -->
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a href="#" class="navbar-brand"><?php echo $siteTitle; ?></a>
-                </div>
-
-                <div class="collapse navbar-collapse navbar-right" id="navmenu">
-                    <ul class="collapse navbar-collapse">
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">services</a></li>
-                        <li><a href="#">Testimonials</a></li>
-                        <li><a href="#">Gallery</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                    </ul>
-                </div>
-            </div>
-
-            
-        </nav>
+        <?php include('includes/nav.php'); ?>
 
         <!-- Add your site or application content here -->
         
