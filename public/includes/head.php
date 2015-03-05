272f9ecb2eb0ccad0cde91573c159102a27dccd2
matt trask <mjftrask@gmail.com>
2015-03-05 17:19:16 -0500

    added js tag for main.js

--- public/includes/head.php
+++ public/includes/head.php
@@ -28,9 +28,10 @@
         
         <!-- Scripts -->
         <script src="js/vendor/modernizr-2.8.0.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script src="//cdn.rawgit.com/noelboss/featherlight/1.0.4/release/featherlight.min.js" type="text/javascript" charset="utf-8"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBBuETsf3EeVod_t1eW1kVySHXvR_ggOeE"></script>
+        <script type="text/javascript" src="js/main.js"></script>
 
     </head>
