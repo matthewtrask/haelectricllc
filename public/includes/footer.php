c68a3b8028a85de22597bd1c4b47e46b05497cf9
matt trask <mjftrask@gmail.com>
2015-03-05 17:12:45 -0500

    fixed responsiveness with the credit card images

--- public/includes/footer.php
+++ public/includes/footer.php
@@ -2,7 +2,11 @@
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <h2 class="text-center">Call us now to schedule a consultation! <span id="footerPhone">678-527-8292</span></h2>
-            <h4 class="text-center">We proudly accept: <i class="fa fa-cc-visa fa-2x" id="cc"></i> <i class="fa fa-cc-mastercard fa-2x" id="cc"></i> <i class="fa fa-cc-discover fa-2x" id="cc"></i> <i class="fa fa-cc-amex fa-2x" id="cc"></i></h4>
+            <div class="row">    
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <h4 class="text-center">We proudly accept: <i class="fa fa-cc-visa fa-lg" id="cc"></i> <i class="fa fa-cc-mastercard fa-lg" id="cc"></i> <i class="fa fa-cc-discover fa-lg" id="cc"></i> <i class="fa fa-cc-amex fa-lg" id="cc"></i></h4>
+                </div>
+            </div>
         </div>
     </div>
 </div>
