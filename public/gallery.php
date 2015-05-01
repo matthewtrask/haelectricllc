894690960658dd72f26b92c61e8fc711b0c6adfb
matt trask <mjftrask@gmail.com>
2015-05-01 10:01:46 -1000

    updated all alt tags on images for google search

--- public/gallery.php
+++ public/gallery.php
@@ -9,35 +9,16 @@ include('includes/nav.php');?>
 
         <div class="row" id="pics">
             <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <img src="img/bbqdiningroom.jpg" class="img-responsive center-block" id="mylightbox" data-featherlight="img/bbqdiningroom.jpg">
+                <img src="img/bbqdiningroom.jpg" alt="HA Electric LLC Restaurant Light Electric Lighting BBQ Dining Room" class="img-responsive center-block" id="mylightbox" data-featherlight="img/bbqdiningroom.jpg">
             </div>
             <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <img src="img/memphisbar.jpg" class="img-responsive center-block" id="mylightbox" data-featherlight="img/memphisbar.jpg">
+                <img src="img/memphisbar.jpg" alt="HA Electric LLC Memphis BBQ LED Bar Lighting" class="img-responsive center-block" id="mylightbox" data-featherlight="img/memphisbar.jpg">
             </div>
             <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <img src="img/lighting1.jpg" class="img-responsive center-block" id="myLightbox" data-featherlight="img/lighting1.jpg">
+                <img src="img/lighting1.jpg" alt="HA Electric LLC A Frame Bar LED Lighting" class="img-responsive center-block" id="myLightbox" data-featherlight="img/lighting1.jpg">
             </div>
             <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <img src="img/spicebar.jpg" class="img-responsive center-block" id="myLightbox" data-featherlight="img/spicebar.jpg">
-            </div>
-        </div>
-
-    </div>
-
-    <div class="container">
-
-        <div class="row" id="pics">
-            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <img src="img/tabletops.jpg" class="img-responsive center-block" id="myLightbox" data-featherlight="img/tabletops.jpg">
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <img src="img/glasspane.jpg" class="img-responsive center-block" id="myLightbox" data-featherLight="img/glasspane.jpg">
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <img src="img/outdoorbar.jpg" class="img-responsive center-block" id="myLightbox" data-featherLight="img/outdoorbar.jpg">
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <img src="img/bluetables.jpg" class="img-responsive center-block" id="myLightbox" data-fatherLight="img/bluetables.jpg">
+                <img src="img/spicebar.jpg" alt="HA Electric LLC Spice Bar And Brick Oven Pizza LED Bar Lighting" class="img-responsive center-block" id="myLightbox" data-featherlight="img/spicebar.jpg">
             </div>
         </div>
 
@@ -47,16 +28,35 @@ include('includes/nav.php');?>
 
         <div class="row" id="pics">
             <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <img src="img/aviary1.jpg" class="img-responsive center-block" id="myLightbox" data-featherLight="img/aviary1.jpg">
+                <img src="img/tabletops.jpg" alt="HA Electric LLC Bar Tables LED Infused Tables" class="img-responsive center-block" id="myLightbox" data-featherlight="img/tabletops.jpg">
             </div>
             <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <img src="img/hangingbarlights.jpg" class="img-responsive center-block" id="myLightbox" data-featherLight="img/hangingbarlights.jpg">
+                <img src="img/glasspane.jpg" alt="HA Electric LLC LED Gradient Lighting" class="img-responsive center-block" id="myLightbox" data-featherLight="img/glasspane.jpg">
             </div>
             <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <img src="img/litdrink.jpg" class="img-responsive center-block" id="myLightbox" data-featherLight="img/litdrink.jpg">
+                <img src="img/outdoorbar.jpg" alt="HA Electric LLC Infused Brick LED Columns Lighting" class="img-responsive center-block" id="myLightbox" data-featherLight="img/outdoorbar.jpg">
             </div>
             <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                <img src="img/litbar.jpg" class="img-responsive center-block" id="myLightbox" data-featherLight="img/litbar.jpg">
+                <img src="img/bluetables.jpg"alt="HA Electric LLC LED Bar Top and Table Tops Lighting" class="img-responsive center-block" id="myLightbox" data-fatherLight="img/bluetables.jpg">
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <div class="row" id="pics">
+            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                <img src="img/aviary1.jpg" alt="HA Electric LLC Mirror Lighting Hair Salon Lights" class="img-responsive center-block" id="myLightbox" data-featherLight="img/aviary1.jpg">
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                <img src="img/hangingbarlights.jpg" alt="HA Electric LLC Hanging LED Lights" class="img-responsive center-block" id="myLightbox" data-featherLight="img/hangingbarlights.jpg">
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                <img src="img/litdrink.jpg" alt="HA Electric LLC LED Lights through a bar glass" class="img-responsive center-block" id="myLightbox" data-featherLight="img/litdrink.jpg">
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                <img src="img/litbar.jpg" alt="HA Electric LLC LED Bar Panels imbedded in a bar" class="img-responsive center-block" id="myLightbox" data-featherLight="img/litbar.jpg">
             </div>
         </div>
 
